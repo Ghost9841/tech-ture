1cf64d66c8994b61c0ea3bb7556e84c0ba560dc6
Ghost9841 <asubedi778@gmail.com>
2025-05-26 16:09:26 +0545

    Configurations are finally done of that tailwindcss v4 is really tiring but i think i have learned somehow what to do and how to do

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 type Props = {}
 
 const App = (props: Props) => {
   return (
     <>
-
+      <main className="relative min-h-screen overflow-x-hidden">
+        <Hero />
+      </main>
     </>
   )
 }
